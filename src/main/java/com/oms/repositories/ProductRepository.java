d85d42a95ca4c25d44da02970ccc112d48521725
nisum <nisum@192.168.1.136>
2017-01-11 20:01:18 +0530

    added add new Item

--- src/main/java/com/oms/repositories/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.oms.repositories;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.oms.pojo.Product;
-
-public interface ProductRepository extends MongoRepository<Product, String> {
-
-}
